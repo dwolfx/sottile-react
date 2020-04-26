3f9ec085cf8f8a9ecf4ccd3ae09c1c999ae4a456
Victor 'dWolf' Morais <victor9009@gmail.com>
2020-04-25 21:32:54 -0300

    build after contact

--- build/precache-manifest.e646620d17c6d5a916cbc7f7d9a5bf43.js
+++ build/precache-manifest.e646620d17c6d5a916cbc7f7d9a5bf43.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7742e56de10e6fe504ad397081af299a",
+    "revision": "ab8a542a9932125cb96d30007956e56b",
     "url": "/sottile-react/index.html"
   },
   {
-    "revision": "a90b839ba42834a0ab9b",
+    "revision": "9fb77ceba3c8260c6786",
     "url": "/sottile-react/static/css/main.70fc5191.chunk.css"
   },
   {
-    "revision": "48537bb99aef788f0552",
-    "url": "/sottile-react/static/js/2.14ef0aa7.chunk.js"
+    "revision": "e9fea33a590c93caf68f",
+    "url": "/sottile-react/static/js/2.125dc98b.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/sottile-react/static/js/2.14ef0aa7.chunk.js.LICENSE.txt"
+    "url": "/sottile-react/static/js/2.125dc98b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a90b839ba42834a0ab9b",
-    "url": "/sottile-react/static/js/main.792df972.chunk.js"
+    "revision": "9fb77ceba3c8260c6786",
+    "url": "/sottile-react/static/js/main.6f55ef9c.chunk.js"
   },
   {
     "revision": "872647b62c31f01eb572",
