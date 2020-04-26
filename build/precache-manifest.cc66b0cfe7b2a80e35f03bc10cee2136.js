7233b2b065cad91c1b0aee558a361dbc37e2d950
Victor 'dWolf' Morais <victor9009@gmail.com>
2020-04-25 21:36:31 -0300

    upload new build files

---
+++ build/precache-manifest.cc66b0cfe7b2a80e35f03bc10cee2136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ca800dcdb24aed6de3dee0f268d04b",
+    "url": "/sottile-react/index.html"
+  },
+  {
+    "revision": "06db805ffdcd25997c1d",
+    "url": "/sottile-react/static/css/main.70fc5191.chunk.css"
+  },
+  {
+    "revision": "e9fea33a590c93caf68f",
+    "url": "/sottile-react/static/js/2.125dc98b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sottile-react/static/js/2.125dc98b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06db805ffdcd25997c1d",
+    "url": "/sottile-react/static/js/main.2cc63e88.chunk.js"
+  },
+  {
+    "revision": "872647b62c31f01eb572",
+    "url": "/sottile-react/static/js/runtime-main.2a985eb3.js"
+  }
+]);
